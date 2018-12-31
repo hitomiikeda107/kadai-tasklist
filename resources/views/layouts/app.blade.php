f89f902d70813b8c2bbfc664cce5cff5bd371b28
hitomi.ikeda <tech.hitomi.shiga@gmail.com>
2018-12-31 07:30:37 +0000

    add column status

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,10 +8,10 @@
     </head>
 
     <body>
-        
         @include('commons.navbar')
         
         <div class="container">
+            @include("commons.error_messages")
             @yield('content')
         </div>
         
